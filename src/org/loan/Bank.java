3b14ea897da2fb6c85d702b682c7ca5f689f3d31
dhana191995 <dhanalakshmigovindhan06@gmail.com>
2019-10-18 09:16:38 +0530

    first commit

---
+++ src/org/loan/Bank.java
@@ -0,0 +1,11 @@
+package org.loan;
+
+public abstract class Bank {
+	public abstract void axis();
+	public abstract void icici();
+	public abstract void hdfc();
+public void sbi() {
+	// TODO Auto-generated method stub
+System.out.println("sbi bank");
+}	
+}
